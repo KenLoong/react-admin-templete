9041db96422526ff530df07a865696e5ac3d3f52
warson <...>
2024-11-01 18:29:33 +0800

    完成home页面渲染

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,23 +1,43 @@
-import React from 'react'
-import { Card } from 'antd'
+import React, { useEffect, useState } from 'react';
+import { Card, List, Typography } from 'antd';
 import "./home.css"
 
 const Home = () => {
-  const userImg = require("../../assets/images/user.png")
+  const [userInfo, setUserInfo] = useState(null);
+
+  useEffect(() => {
+    const userString = localStorage.getItem('user_info');
+    if (userString) {
+      const user = JSON.parse(userString);
+      setUserInfo(user);
+    }
+  }, []);
+
+  if (!userInfo) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="home">
-      <Card className="user-card" hoverable>
-        <div className="user">
-          <img src={userImg} alt="User" />
-          <div className="userinfo">
-            <p className="name">Admin</p>
-            <p className="access">超级管理员</p>
-          </div>
-        </div>
+      <Card title="User Information" style={{ marginBottom: 16 }}>
+        <Typography.Text strong>Username:</Typography.Text> {userInfo.username}
+        <br />
+        <Typography.Text strong>Roles:</Typography.Text> {userInfo.roles.map(role => role.name).join(', ')}
+        <br />
+        <Typography.Text strong>Permissions:</Typography.Text>
+        <List
+          size="small"
+          bordered
+          dataSource={userInfo.permissions}
+          renderItem={item => (
+            <List.Item>
+              {item.name} ({item.type})
+            </List.Item>
+          )}
+        />
       </Card>
     </div>
-  )
-}
+  );
+};
 
 export default Home
